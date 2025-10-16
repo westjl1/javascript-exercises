3096443abc40fcfcc1cc9b2964d9b6ee543f380e
Jeremy West <westjl@comcast.net>
2025-10-15 18:05:16 -0700

    loops and arrays 08 remove from array

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function (numbers, ...removes) {
+  return numbers.filter((num) => !removes.includes(num));
 };
 
 // Do not edit below this line
