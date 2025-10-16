edeea4b9f35f2accb501e404aa43438280bd44e4
Jeremy West <westjl@comcast.net>
2025-10-15 17:31:01 -0700

    loops and arrays assignment repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function (inString, repeatNum) {
+  let returnString = "";
 
+  if (repeatNum < 0) {
+    return "ERROR";
+  }
+  for (let i = 0; i < repeatNum; i++) {
+    returnString += inString;
+  }
+  return returnString;
 };
 
 // Do not edit below this line
