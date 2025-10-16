0d12ac7a5631cc54b91768acb503b1aaaa79cdce
Jeremy West <westjl@comcast.net>
2025-10-15 18:42:28 -0700

    loops and array 10 sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  let retVal = 0;
 
+  if (!Number.isInteger(a) || !Number.isInteger(b) || a < 0 || b < 0) {
+    return "ERROR";
+  }
+
+  if (a < b) {
+    for (let i = a; i <= b; i++) {
+      retVal += i;
+    }
+  } else if (a > b) {
+    for (let i = b; i <= a; i++) {
+      retVal += i;
+    }
+  } else {
+    retVal = a + b;
+  }
+  return retVal;
 };
 
 // Do not edit below this line
