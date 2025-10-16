1d75d7216683b6b34ab3dc3bbef83c35f86b5af2
Jeremy West <westjl@comcast.net>
2025-10-16 08:06:37 -0700

    loops and arrays 11 tempConversion

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,11 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (tempF) {
+  return +((tempF - 32) / 1.8).toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (tempC) {
+  return +(tempC * 1.8 + 32).toFixed(1);
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
