3039b209a8582f37d16d84d08d5c1d64d61fb530
Jeremy West <westjl@comcast.net>
2025-10-15 17:48:07 -0700

    loops and arrays 07 reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+//reverseString('hello there') // returns 'ereht olleh'
 
+const reverseString = function (inString) {
+  let retString = "";
+  for (let i = inString.length; i >= 0; i--) {
+    retString += inString.charAt(i);
+  }
+  return retString;
 };
 
 // Do not edit below this line
