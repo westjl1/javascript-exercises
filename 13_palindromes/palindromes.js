f971c4e93bd7feda0b55dd22f4a8c350b187cc90
Jeremy West <westjl@comcast.net>
2025-10-20 20:06:37 -0700

    13 palindromes complete

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
+const reverseString = require("../07_reverseString/reverseString.js");
 
+const palindromes = function (checkStr) {
+  //A car, a man, a maraca.
+  //.acaram a ,nam a ,rac A
+  //   console.log(checkStr);
+  //   console.log(reverseString(checkStr));
+  //   console.log(checkStr === reverseString(checkStr));
+
+  const alphanumerical = "abcdefghijklmnopqrstuvwxyz0123456789";
+
+  // Convert to lowercase, split to array of individual characters,
+  // filter only valid characters, then rejoin as new string
+  const cleanedCheckStr = checkStr
+    .toLowerCase()
+    .split("")
+    .filter((character) => alphanumerical.includes(character))
+    .join("");
+
+  return cleanedCheckStr === reverseString(cleanedCheckStr);
 };
 
 // Do not edit below this line
